70ff0d9ea1e4f02e1bb4d75760b52e4770928201
xyy0524 <1197041744@qq.com>
2024-02-20 19:30:29 +0800

    增加活动实体类

---
+++ xhlj-model/src/main/java/com/swyxl/model/entity/active/activeInfo.java
@@ -0,0 +1,31 @@
+package com.swyxl.model.entity.active;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.swyxl.model.entity.base.BaseEntity;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Schema(description = "活动信息表")
+public class activeInfo extends BaseEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    @Schema(description = "名称")
+    private String name;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @Schema(description = "日期")
+    private Data date;
+
+    @Schema(description = "详情")
+    private String detail;
+
+    @Schema(description = "位置")
+    private String location;
+
+    @Schema(description ="图片")
+    private String image;
+
+    @Schema(description = "价格")
+    private int price;
+}
